8f631612b18d34f3fdc7afd66f21562a37ffe637
iMrihan <mhd.khan.rihan@gmail.com>
2022-10-01 18:08:48 +0530

    added increment and decrement counter

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button @click="increment">Increment</button>
+    {{ count }}
+
+    <button @click="decrement">Decrement</button>
   </div>
 </template>
+
+<script setup>
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  if (count.value >= 1) {
+    count.value--;
+  }
+}
+</script>
